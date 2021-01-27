3497fd91027434a5fb4f940e48c464a76a923ad2
lucyfer3k <lucyfer3k@gmail.com>
2021-01-27 14:42:21 +0100

    Remove token

--- pingmon.sh
+++ pingmon.sh
@@ -3,20 +3,20 @@ DATEPRT=$(date)
 
 #Cloudflare
 curl --request POST "http://influxdb:8086/api/v2/write?org=MW&bucket=netstats" \
-    --header "Authorization: Token zRKFVzjb-xeUrC4-81L1CQvVQ_OqPcXP4c4cmKqjVKtGXFOz0ka22aVP_-ZN2mkuhG4lURrKneLI5KcNjjgIBw==" \
+    --header "Authorization: Token XXX" \
     --data-binary "ping,dest=cloudflare $(ping -c 1 1.1.1.1 | \
     awk '/time=/{print $7}' | sed -e 's/time/rtt/g') $(date +%s%N)"
 
 #Google
 curl --request POST "http://influxdb:8086/api/v2/write?org=MW&bucket=netstats" \
-    --header "Authorization: Token zRKFVzjb-xeUrC4-81L1CQvVQ_OqPcXP4c4cmKqjVKtGXFOz0ka22aVP_-ZN2mkuhG4lURrKneLI5KcNjjgIBw==" \
+    --header "Authorization: Token XXX" \
     --data-binary "ping,dest=google $(ping -c 1 8.8.4.4 | \
     awk '/time=/{print $7}' | sed -e 's/time/rtt/g') $(date +%s%N)"
 
 #Chello-Warsaw
 curl --request POST "http://influxdb:8086/api/v2/write?org=MW&bucket=netstats" \
-    --header "Authorization: Token zRKFVzjb-xeUrC4-81L1CQvVQ_OqPcXP4c4cmKqjVKtGXFOz0ka22aVP_-ZN2mkuhG4lURrKneLI5KcNjjgIBw==" \
+    --header "Authorization: Token XXX" \
     --data-binary "ping,dest=chello-warsaw $(ping -c 1 83.144.93.10 | \
     awk '/time=/{print $7}' | sed -e 's/time/rtt/g') $(date +%s%N)"
 
-echo "$DATEPRT: pingmon is a success"
+echo "$DATEPRT: pingmon was successful"

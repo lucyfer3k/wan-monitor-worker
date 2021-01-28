e5f6e8ed6426805abfd0ca649dfb8b20e291f51a
Mateusz Włodarczyk <lucyfer3k@gmail.com>
2021-01-28 16:24:24 +0100

    Remove elasticsearch from the process, gather speedtest data through InfluxDB

--- worker-wanmon/speedmon.sh
+++ worker-wanmon/speedmon.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 set -eo pipefail
 
-SPEEDTEST_JSON=$(speedtest-cli --json)
-DATEFMT=$(date +%Y.%V)
+echo $(speedtest-cli --csv) > ./speedtest_last_result.csv
 
-curl -H "Content-Type: application/json" \
-    -XPOST "http://elasticsearch:9200/speedtest-$DATEFMT/doc" \
-    -d "$SPEEDTEST_JSON" >> /dev/null
+Download=$(cat ./speedtest_last_result.csv | cut -d ',' -f7)
+Upload=$(cat ./speedtest_last_result.csv | cut -d ',' -f8)
 
-echo "$(date): speedtest was successful"
+
+curl --request POST "http://influxdb:8086/api/v2/write?org=$INFLUXDB_ORG&bucket=$INFLUXDB_BUCKET" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Token $INFLUXDB_TOKEN" \
+    --data-binary "speedtest,host=worker download=$Download,upload=$Upload $(date +%s%N)"

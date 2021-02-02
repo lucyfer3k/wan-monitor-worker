a8eef27e539dbb15e373b0f7b689acf1599d2f28
Mateusz Włodarczyk <lucyfer3k@gmail.com>
2021-02-02 12:07:56 +0100

    Fixed problem with InfluxDB

--- worker/speedmon.sh
+++ worker/speedmon.sh
@@ -14,7 +14,7 @@ do
   Upload=$(cat ./speedtest_last_result.csv | cut -d ',' -f8)
 
 
-  curl -s --request XPOST "http://influxdb:8086/api/v2/write?org=$INFLUXDB_ORG&bucket=$INFLUXDB_BUCKET" \
+  curl -s --request POST "http://influxdb:8086/api/v2/write?org=$INFLUXDB_ORG&bucket=$INFLUXDB_BUCKET" \
       --header "Content-Type: application/json" \
       --header "Authorization: Token $INFLUXDB_TOKEN" \
       --data-binary "speedtest,host=worker download=$Download,upload=$Upload $(date +%s%N)"
